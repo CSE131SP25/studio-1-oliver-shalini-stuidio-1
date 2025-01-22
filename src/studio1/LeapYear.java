af083d74c5f665c3b5f95c03d69fc909489a53fc
yukaigu <yukaigu@localhost>
2025-01-22 15:45:28 -0600

    123

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,16 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is the year?");
+		int x = in.nextInt();
+		boolean leapyear = (x % 4 == 0) && (x % 100 > 0) || (x % 400 == 0);
+		System.out.println(leapyear);
+	}
+
+}
