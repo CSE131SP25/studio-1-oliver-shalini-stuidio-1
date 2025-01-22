3f39c229d25e807b4ba028a80179f375ceca08a7
yukaigu <yukaigu@localhost>
2025-01-22 15:45:12 -0600

    Oliver Gu

--- src/studio1/Average.java
+++ src/studio1/Average.java
@@ -1,8 +1,17 @@
 package studio1;
 
+import java.util.Scanner;
+
 public class Average {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("the first integer to be averaged");
+		int n1 = in.nextInt();
+		System.out.print("the second integer to be averaged");
+		int n2 = in.nextInt();
+		double average = ((n1 + n2)/2);
+		System.out.print("the average is" + average);
 		// TODO Auto-generated method stub
 
 	}
